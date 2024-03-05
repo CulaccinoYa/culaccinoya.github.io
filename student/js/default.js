f5a78be967abcdfbc83189a42fc84359100ef917
CulaccinoYa <136312945+CulaccinoYa@users.noreply.github.com>
2024-03-05 18:22:21 +0800

    Add files via upload

--- student/js/default.js
+++ student/js/default.js
@@ -7,7 +7,59 @@ jconfirm.defaults={
 		cancel:{text:"取消"}
 	}
 }
-var Host="https://sf-api.suxber.live:2053/"
+var Host="https://api.sftools.link:2053/"
+function ajax(data,suc=function(i){},fail=function(i){}){
+	if(!data.loading){
+		data.loading='操作正在处理'
+	}
+	if(!data.timeout){
+		data.timeout=15000
+	}
+	if(!data.hideload){
+		var loading=$.alert({content:'<div class="p-2 d-flex align-items-center justify-content-between"> <strong>'+data.loading+'</strong> <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div></div>',buttons:[],closeIcon:false});
+	}
+	let user=$.cookie('student_id');
+	let token=$.cookie('student_token');
+	$.ajax({
+		type:"POST",
+		url:Host+data.url,
+		data:JSON.stringify({"data":data.data,"account":{"user":user,"token":token}}),
+		timeout:data.timeout,
+		contentType:"application/json;charset=UTF-8",
+		success:function (e) {
+			if(!data.hideload){
+				loading.close();
+			}
+			if(e.code==200){
+				suc(e.data);
+			}else if(e.code==1000){
+				$.alert({type:'red',icon:"bi bi-x-circle-fill",title:'错误',content:'账户信息发生变动，您被迫下线',buttons:{ok:function(){window.location.href='./login.html'}},closeIcon:false});
+			}else if(e.code==300){
+				window.location.href=e.data
+				fail({network:true,msg:'请在弹出的新窗口继续完成操作'})
+			}else if(e.code==301){
+				if(!data.hidefail){
+					$.alert({title:'请继续完成操作',content:'<iframe style="height:70vh;width:100%" scrolling="0" frameborder="0" src="'+e.data+'"></iframe>',boxWidth:'100%'})
+				}
+				fail({network:true,msg:'请在弹出的新窗口继续完成操作'})
+			}else{
+				if(!data.hidefail){
+					$.alert({type:'red',icon:"bi bi-x-circle-fill",title:'错误',content:e.msg});
+				}
+				fail({network:true,msg:e.msg})
+			}
+		},
+		error:function () {
+			if(!data.hideload){
+				loading.close();
+			}
+			if(!data.hidefail){
+				$.alert({type:'red',icon:"bi bi-x-circle-fill",title:'错误',content:'网络异常，请检查网络连接'});
+			}
+			fail({network:false})
+		}
+	});
+}
 function getvalue(variable) {
 	var query = window.location.search.substring(1);
 	var vars = query.split("&");
@@ -50,7 +102,7 @@ function addWaterMarker() {
     cans.fillStyle = "rgba(0, 0, 0, 0.125)";
     cans.textAlign = 'center';
     cans.textBaseline = 'Middle';
-    cans.fillText($.cookie('student_id')+' - 操行分查询系统', can.width / 3, can.height / 2);
+    cans.fillText($.cookie('student_id')+' - 数字班干', can.width / 3, can.height / 2);
 	let t=new Date(Date.now())
 	var y = t.getFullYear();
 	var m = t.getMonth()+1;
